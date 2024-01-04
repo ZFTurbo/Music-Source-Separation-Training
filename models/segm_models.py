1c735dc1d2a71f08d6d6a1dd661c7bfb305b4dab
user-miner1 <amvnews@mail.ru>
2024-01-05 00:42:39 +0300

    Added support for all decoders for segmentation models

--- models/segm_models.py
+++ models/segm_models.py
@@ -69,6 +69,132 @@ def get_act(act_type):
         raise Exception
 
 
+def get_decoder(config, c):
+    decoder = None
+    decoder_options = dict()
+    if config.model.decoder_type == 'unet':
+        try:
+            decoder_options = dict(config.decoder_unet)
+        except:
+            pass
+        decoder = smp.Unet(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'fpn':
+        try:
+            decoder_options = dict(config.decoder_fpn)
+        except:
+            pass
+        decoder = smp.FPN(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'unet++':
+        try:
+            decoder_options = dict(config.decoder_unet_plus_plus)
+        except:
+            pass
+        decoder = smp.UnetPlusPlus(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'manet':
+        try:
+            decoder_options = dict(config.decoder_manet)
+        except:
+            pass
+        decoder = smp.MAnet(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'linknet':
+        try:
+            decoder_options = dict(config.decoder_linknet)
+        except:
+            pass
+        decoder = smp.Linknet(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'pspnet':
+        try:
+            decoder_options = dict(config.decoder_pspnet)
+        except:
+            pass
+        decoder = smp.PSPNet(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'pspnet':
+        try:
+            decoder_options = dict(config.decoder_pspnet)
+        except:
+            pass
+        decoder = smp.PSPNet(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'pan':
+        try:
+            decoder_options = dict(config.decoder_pan)
+        except:
+            pass
+        decoder = smp.PAN(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'deeplabv3':
+        try:
+            decoder_options = dict(config.decoder_deeplabv3)
+        except:
+            pass
+        decoder = smp.DeepLabV3(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    elif config.model.decoder_type == 'deeplabv3plus':
+        try:
+            decoder_options = dict(config.decoder_deeplabv3plus)
+        except:
+            pass
+        decoder = smp.DeepLabV3Plus(
+            encoder_name=config.model.encoder_name,
+            encoder_weights="imagenet",
+            in_channels=c,
+            classes=c,
+            **decoder_options,
+        )
+    return decoder
+
+
 class Segm_Models_Net(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -85,20 +211,7 @@ class Segm_Models_Net(nn.Module):
 
         self.first_conv = nn.Conv2d(dim_c, c, 1, 1, 0, bias=False)
 
-        if config.model.decoder_type == 'unet':
-            self.unet_model = smp.Unet(
-                encoder_name=config.model.encoder_name,
-                encoder_weights="imagenet",
-                in_channels=c,
-                classes=c,
-            )
-        elif config.model.decoder_type == 'fpn':
-            self.unet_model = smp.FPN(
-                encoder_name=config.model.encoder_name,
-                encoder_weights="imagenet",
-                in_channels=c,
-                classes=c,
-            )
+        self.unet_model = get_decoder(config, c)
 
         self.final_conv = nn.Sequential(
             nn.Conv2d(c + dim_c, c, 1, 1, 0, bias=False),
