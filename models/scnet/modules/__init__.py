01af586059fb707fafa389d6b84efb132461c18b
fmac2000 <frazer.mcpherson@gmail.com>
2024-02-28 11:20:52 +0000

    add scnet

---
+++ models/scnet/modules/__init__.py
@@ -0,0 +1,3 @@
+from models.scnet.modules.dualpath_rnn import DualPathRNN
+from models.scnet.modules.sd_encoder import SDBlock
+from models.scnet.modules.su_decoder import SUBlock
--- utils.py
+++ utils.py
@@ -35,6 +35,11 @@ def get_model_from_config(model_type, config):
         model = MultiMaskMultiSourceBandSplitRNNSimple(
             **config.model
         )
+    elif model_type == 'scnet':
+        from models.scnet import SCNet
+        model = SCNet(
+            **config.model
+        )
     else:
         print('Unknown model: {}'.format(model_type))
         model = None
