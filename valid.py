a2f994eaa41f0d79201d9f16d885e30de5a333f9
user-miner1 <amvnews@mail.ru>
2024-03-09 16:38:31 +0300

    MultiGPU parallel validation added

--- valid.py
+++ valid.py
@@ -10,10 +10,12 @@ from tqdm import tqdm
 import sys
 import os
 import glob
+import copy
 import torch
 import soundfile as sf
 import numpy as np
 import torch.nn as nn
+import multiprocessing
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -21,12 +23,7 @@ warnings.filterwarnings("ignore")
 from utils import demix_track, demix_track_demucs, sdr, get_model_from_config
 
 
-def valid(model, args, config, device, verbose=False):
-    start_time = time.time()
-    model.eval()
-    all_mixtures_path = glob.glob(args.valid_path + '/*/mixture.wav')
-    print('Total mixtures: {}'.format(len(all_mixtures_path)))
-
+def proc_list_of_files(all_mixtures_path, model, args, config, device, verbose=False, is_tqdm=True):
     instruments = config.training.instruments
     if config.training.target_instrument is not None:
         instruments = [config.training.target_instrument]
@@ -39,7 +36,7 @@ def valid(model, args, config, device, verbose=False):
     for instr in config.training.instruments:
         all_sdr[instr] = []
 
-    if not verbose:
+    if is_tqdm:
         all_mixtures_path = tqdm(all_mixtures_path)
 
     for path in all_mixtures_path:
@@ -60,7 +57,7 @@ def valid(model, args, config, device, verbose=False):
                     try:
                         track, sr1 = sf.read(folder + '/{}.wav'.format(instr))
                     except Exception as e:
-                        print('No data for stem: {}. Skip!'.format(instr))
+                        # print('No data for stem: {}. Skip!'.format(instr))
                         continue
                 else:
                     # other is actually instrumental
@@ -68,7 +65,8 @@ def valid(model, args, config, device, verbose=False):
                     track = mix - track
 
                 if args.store_dir != "":
-                    sf.write("{}/{}_{}.wav".format(args.store_dir, os.path.basename(folder), instr), res[instr].T, sr, subtype='FLOAT')
+                    sf.write("{}/{}_{}.wav".format(args.store_dir, os.path.basename(folder), instr), res[instr].T, sr,
+                             subtype='FLOAT')
                 references = np.expand_dims(track, axis=0)
                 estimates = np.expand_dims(res[instr].T, axis=0)
                 sdr_val = sdr(references, estimates)[0]
@@ -81,6 +79,85 @@ def valid(model, args, config, device, verbose=False):
                 all_mixtures_path.set_postfix(pbar_dict)
             except Exception as e:
                 pass
+
+    return all_sdr
+
+
+def valid(model, args, config, device, verbose=False):
+    start_time = time.time()
+    model.eval()
+    all_mixtures_path = glob.glob(args.valid_path + '/*/mixture.wav')
+    print('Total mixtures: {}'.format(len(all_mixtures_path)))
+    print('Overlap: {} Batch size: {}'.format(config.inference.num_overlap, config.inference.batch_size))
+
+    all_sdr = proc_list_of_files(all_mixtures_path, model, args, config, device, verbose, not verbose)
+
+    instruments = config.training.instruments
+    if config.training.target_instrument is not None:
+        instruments = [config.training.target_instrument]
+
+    if args.store_dir != "":
+        out = open(args.store_dir + '/results.txt', 'w')
+        out.write(str(args) + "\n")
+    print("Num overlap: {}".format(config.inference.num_overlap))
+    sdr_avg = 0.0
+    for instr in instruments:
+        sdr_val = np.array(all_sdr[instr]).mean()
+        print("Instr SDR {}: {:.4f}".format(instr, sdr_val))
+        if args.store_dir != "":
+            out.write("Instr SDR {}: {:.4f}".format(instr, sdr_val) + "\n")
+        sdr_avg += sdr_val
+    sdr_avg /= len(instruments)
+    if len(instruments) > 1:
+        print('SDR Avg: {:.4f}'.format(sdr_avg))
+    if args.store_dir != "":
+        out.write('SDR Avg: {:.4f}'.format(sdr_avg) + "\n")
+    print("Elapsed time: {:.2f} sec".format(time.time() - start_time))
+    if args.store_dir != "":
+        out.write("Elapsed time: {:.2f} sec".format(time.time() - start_time) + "\n")
+        out.close()
+
+    return sdr_avg
+
+
+def valid_mp(proc_id, all_mixtures_path, model, args, config, device, return_dict):
+    m1 = model
+    m1.eval()
+    m1.to(device)
+    all_sdr = proc_list_of_files(all_mixtures_path, m1, args, config, device, False, False)
+    return_dict[proc_id] = all_sdr
+    return
+
+def valid_multi_gpu(model, args, config, device_ids, verbose=False):
+    start_time = time.time()
+    all_mixtures_path = glob.glob(args.valid_path + '/*/mixture.wav')
+    print('Total mixtures: {}'.format(len(all_mixtures_path)))
+    print('Overlap: {} Batch size: {}'.format(config.inference.num_overlap, config.inference.batch_size))
+
+    model = model.to('cpu')
+    proc_mixtures = np.array_split(all_mixtures_path, len(device_ids))
+    processes = []
+    return_dict = torch.multiprocessing.Manager().dict()
+    for i, device in enumerate(device_ids):
+        device = 'cuda:{}'.format(device)
+        print('Device: {}. Proc: {}'.format(device, len(proc_mixtures[i])))
+        p = torch.multiprocessing.Process(target=valid_mp, args=(i, proc_mixtures[i], model, args, config, device, return_dict))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()  # wait for all subprocesses to finish
+
+    # print(return_dict)
+
+    all_sdr = dict()
+    for instr in config.training.instruments:
+        all_sdr[instr] = []
+        for i in range(len(device_ids)):
+            all_sdr[instr] += return_dict[i][instr]
+
+    instruments = config.training.instruments
+    if config.training.target_instrument is not None:
+        instruments = [config.training.target_instrument]
 
     if args.store_dir != "":
         out = open(args.store_dir + '/results.txt', 'w')
@@ -142,14 +219,18 @@ def check_validation(args):
         model.load_state_dict(state_dict)
 
     device_ids = args.device_ids
-    if type(device_ids)==int:
-        device = torch.device(f'cuda:{device_ids}')
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
         model = model.to(device)
     else:
-        device = torch.device(f'cuda:{device_ids[0]}')
-        model = nn.DataParallel(model, device_ids=device_ids).to(device)
+        device = 'cpu'
+        print('CUDA is not available. Run validation on CPU. It will be very slow...')
+        model = model.to(device)
 
-    valid(model, args, config, device, verbose=False)
+    if torch.cuda.is_available() and len(device_ids) > 1:
+        valid_multi_gpu(model, args, config, device_ids, verbose=False)
+    else:
+        valid(model, args, config, device, verbose=False)
 
 
 if __name__ == "__main__":
