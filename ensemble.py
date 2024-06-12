680ec780524e17eaa331d88b61cdf603d4c01986
user-miner1 <amvnews@mail.ru>
2024-06-12 09:43:09 +0300

    Fixed librosa warnings and transpose errors.

--- ensemble.py
+++ ensemble.py
@@ -11,8 +11,8 @@ import argparse
 def stft(wave, nfft, hl):
     wave_left = np.asfortranarray(wave[0])
     wave_right = np.asfortranarray(wave[1])
-    spec_left = librosa.stft(wave_left, nfft, hop_length=hl)
-    spec_right = librosa.stft(wave_right, nfft, hop_length=hl)
+    spec_left = librosa.stft(wave_left, n_fft=nfft, hop_length=hl)
+    spec_right = librosa.stft(wave_right, n_fft=nfft, hop_length=hl)
     spec = np.asfortranarray([spec_left, spec_right])
     return spec
 
@@ -86,7 +86,7 @@ def average_waveforms(pred_track, weights, algorithm):
         elif algorithm in ['median_wave', 'min_wave', 'max_wave']:
             mod_track.append(pred_track[i])
         elif algorithm in ['avg_fft', 'min_fft', 'max_fft', 'median_fft']:
-            spec = stft(pred_track[i], 2048, 1024)
+            spec = stft(pred_track[i], nfft=2048, hl=1024)
             if algorithm in ['avg_fft']:
                 mod_track.append(spec * weights[i])
             else:
@@ -97,29 +97,29 @@ def average_waveforms(pred_track, weights, algorithm):
         pred_track = pred_track.sum(axis=0)
         pred_track /= np.array(weights).sum().T
     elif algorithm in ['median_wave']:
-        pred_track = np.median(pred_track, axis=0).T
+        pred_track = np.median(pred_track, axis=0)
     elif algorithm in ['min_wave']:
         pred_track = np.array(pred_track)
-        pred_track = lambda_min(pred_track, axis=0, key=np.abs).T
+        pred_track = lambda_min(pred_track, axis=0, key=np.abs)
     elif algorithm in ['max_wave']:
         pred_track = np.array(pred_track)
-        pred_track = lambda_max(pred_track, axis=0, key=np.abs).T
+        pred_track = lambda_max(pred_track, axis=0, key=np.abs)
     elif algorithm in ['avg_fft']:
         pred_track = pred_track.sum(axis=0)
         pred_track /= np.array(weights).sum()
-        pred_track = istft(pred_track, 1024, final_length).T
+        pred_track = istft(pred_track, 1024, final_length)
     elif algorithm in ['min_fft']:
         pred_track = np.array(pred_track)
         pred_track = lambda_min(pred_track, axis=0, key=np.abs)
-        pred_track = istft(pred_track, 1024, final_length).T
+        pred_track = istft(pred_track, 1024, final_length)
     elif algorithm in ['max_fft']:
         pred_track = np.array(pred_track)
         pred_track = absmax(pred_track, axis=0)
-        pred_track = istft(pred_track, 1024, final_length).T
+        pred_track = istft(pred_track, 1024, final_length)
     elif algorithm in ['median_fft']:
         pred_track = np.array(pred_track)
         pred_track = np.median(pred_track, axis=0)
-        pred_track = istft(pred_track, 1024, final_length).T
+        pred_track = istft(pred_track, 1024, final_length)
     return pred_track
 
 
