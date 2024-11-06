932ff01babd112abaa53c99037c222508ed9946e
KaiMikkelsen <squabee@gmail.com>
2024-11-06 16:04:49 -0500

    musdb on requirements

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #SBATCH --time=01:00:00
 #SBATCH --account=kaim
+
+echo 'loading venv'
+module load python/3.10
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install -r requirements.txt
+
 echo 'running pipeline script'
 python run_pipeline.py
