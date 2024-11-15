ef0e5825e805a1f4f5f4d5749e7675c8fb9c0a0f
KaiMikkelsen <squabee@gmail.com>
2024-11-15 14:51:00 -0500

    more memory on cedar

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --mem=125G         # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=2-00:00     # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
-
-# echo 'loading venv'
-# module load python/3.10
-# virtualenv --no-download $SLURM_TMPDIR/env
-# source $SLURM_TMPDIR/env/bin/activate
-# pip install --no-index --upgrade pip
-
-#pip install -r requirements.txt
 
 source separation_env/bin/activate
 
