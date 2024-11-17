6cd77e5f34063d2d3cf16f5505f23a006c7ac866
KaiMikkelsen <squabee@gmail.com>
2024-11-17 11:01:36 -0500

    use gpu

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,6 +5,9 @@
 #SBATCH --time=2-00:00     # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 
+module load python/3.10 cuda/12.2 cudnn/8.9.5.29 module load python/3.10
+ 
+
 source separation_env/bin/activate
 
 echo 'running pipeline script'
