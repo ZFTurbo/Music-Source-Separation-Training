d57a405e2c87f7aee32dbde5b3c8d6c0c968996a
KaiMikkelsen <squabee@gmail.com>
2024-11-20 11:42:56 -0500

    test

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=125G         # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=2-00:00     # DD-HH:MM:SS
+#SBATCH --time=0-10:00     # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 
 module load python/3.10 cuda/12.2 cudnn/8.9.5.29
