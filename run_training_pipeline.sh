2610261bcfc6b6ebaa1f8b36f218133851961d52
KaiMikkelsen <squabee@gmail.com>
2024-11-21 11:33:02 -0500

    different training script

--- run_training_pipeline.sh
+++ run_training_pipeline.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100:4
-#SBATCH --ntasks-per-node=24
-#SBATCH --exclusive
-#SBATCH --mem=125G
-#SBATCH --time=0-00:03
+#SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --mem=125G         # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=0-00:03     # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 #SBATCH --output=my_job_output.out  # Save the output to this file
 #SBATCH --error=my_job_error.err    # Save the error to this file
 
 module load python/3.10 cuda/12.2 cudnn/8.9.5.29
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
+
+
  
 
 source separation_env/bin/activate
