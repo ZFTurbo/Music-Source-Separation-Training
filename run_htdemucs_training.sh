35f13d39f500e7ff8c43dc76455ff262fb77e171
KaiMikkelsen <squabee@gmail.com>
2024-12-09 16:26:49 -0500

    new results

--- run_htdemucs_training.sh
+++ run_htdemucs_training.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=125G         # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=1-00:00     # DD-HH:MM:SS
+#SBATCH --time=1-00:60     # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 #SBATCH --output=my_job_output_htdemucs.out  # Save the output to this file
 #SBATCH --error=my_job_error_htdemucs.err    # Save the error to this file
@@ -10,11 +10,19 @@
 module load python/3.10 cuda/12.2 cudnn/8.9.5.29
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
 
+CURRENT_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
+# Create a dynamic results folder
+RESULTS_PATH="results/htdemucs_$CURRENT_DATE"
+
+# Ensure the results directory exists
+mkdir -p "$RESULTS_PATH"
+
+echo "Results will be saved to $RESULTS_PATH"
  
 
 source separation_env/bin/activate
 
 echo 'running training script'
-# python run_training.py --model_type "mdx23c" --config_path="configs/config_musdb18_mdx23c.yaml" --results_path="results/mdx23c" --start_check_point="results/mdx23c.ckpt"
-python run_training.py
+python run_training.py --model_type "htdemucs" --config_path="configs/config_musdb18_htdemucs.yaml" --results_path="$RESULTS_PATH" --start_check_point=""
+#python run_training.py
--- run_mdx23c_training.sh
+++ run_mdx23c_training.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=125G         # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=1-00:00     # DD-HH:MM:SS
+#SBATCH --time=1-00:60     # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 #SBATCH --output=my_job_output_mdx23c.out  # Save the output to this file
 #SBATCH --error=my_job_error_mdx23c.err    # Save the error to this file
@@ -11,10 +11,20 @@ module load python/3.10 cuda/12.2 cudnn/8.9.5.29
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
 
 
+CURRENT_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Create a dynamic results folder
+RESULTS_PATH="results/mdx23c_$CURRENT_DATE"
+
+# Ensure the results directory exists
+mkdir -p "$RESULTS_PATH"
+
+echo "Results will be saved to $RESULTS_PATH"
+
  
 
 source separation_env/bin/activate
 
 echo 'running training script'
-python run_training.py --model_type "mdx23c" --config_path="configs/config_musdb18_mdx23c.yaml" --results_path="results/mdx23c" --start_check_point=""
+python run_training.py --model_type "mdx23c" --config_path="configs/config_musdb18_mdx23c.yaml" --results_path="$RESULTS_PATH" --start_check_point=""
 #python run_training.py
