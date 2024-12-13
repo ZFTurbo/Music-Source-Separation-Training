343fd7f6abfe7e8dc8a7eea42d4ad20939def7a5
user-miner1 <amvnews@mail.ru>
2024-12-13 18:49:27 +0300

    use_amp fix

--- utils.py
+++ utils.py
@@ -223,9 +223,7 @@ def demix(
 
     batch_size = config.inference.batch_size
 
-    use_amp = True
-    if 'use_amp' in config.training:
-        use_amp = config.training['use_amp']
+    use_amp = getattr(config.training, 'use_amp', True)
 
     with torch.cuda.amp.autocast(enabled=use_amp):
         with torch.inference_mode():
