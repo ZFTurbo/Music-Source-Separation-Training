05d5a0da55a7d0fe88cd8c3565449b7d81f99e5a
KaiMikkelsen <squabee@gmail.com>
2024-12-18 10:44:07 -0500

    3 days of training

--- run_htdemucs_training.sh
+++ run_htdemucs_training.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6        # Adjust based on your cluster's CPU/GPU ratio
 #SBATCH --mem=125G               # Adjust memory as needed
-#SBATCH --time=7-00:00           # DD-HH:MM:SS
+#SBATCH --time=3-00:00           # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 #SBATCH --output=slurm_logs/slurm-%j.out  # Use Job ID for unique output files
 
